9dd7580809f7883a3c5c7665c6c91e8c580dccf0
Sharad Shinde <shindesharad71@gmail.com>
2017-07-29 15:25:15 +0530

    code init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div> 
+    <app-nav></app-nav>
+</div>
+</template>
+
+<script>
+import Nav from './components/Nav.vue';
+
+export default {
+  components: {
+    'app-nav': Nav
+  },
+  data() {
+    return {
+
+    };
+  }
+}
+</script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,74 @@
+<template>
+    <!-- Static navbar -->
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">
+                <img style="max-width:40px; margin-top: -7px;" src="https://bookaholicc.com/images/logo_new.png">
+            </a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Product Catalogue <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+            <ul class="nav navbar-nav">
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Product Catalogue 2<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="navbar-form navbar-left">
+  <div class="input-group">
+    <input type="text" class="form-control" placeholder="Search">
+    <div class="input-group-btn">
+      <button class="btn btn-default" type="submit">
+        <i class="glyphicon glyphicon-search"></i>
+      </button>
+    </div>
+  </div>
+</form>
+            <ul class="nav navbar-nav navbar-right">
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">EU<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">AB</a></li>
+                  <li><a href="#">CD</a></li>
+                  <li><a href="#">INR</a></li>
+                  <li><a href="#">$</a></li>
+                </ul>
+              </li>
+              <li><i class="glyphicon glyphicon-shopping-cart"></i>Total<br>123</li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
+</template>
+
+<style scoped>
+.navbar {
+    border-radius: 0px;
+    background-color: #000;
+    border-color: #000;
+}
+</style>
